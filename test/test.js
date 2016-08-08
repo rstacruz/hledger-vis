c3e9699d4c3bb1fe826d4dc22cd064c56a4570a1
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-08-08 14:22:40 +0800

    Update

--- test/test.js
+++ test/test.js
@@ -4,13 +4,13 @@ const toQuery = Query.toQuery
 const normalize = Query.normalize
 
 test('Query.toQuery', t => {
-  t.deepEqual(toQuery('acct:Assets'), [
+  t.deepEqual(toQuery({q: 'acct:Assets'}), [
     { type: 'key', key: 'acct', value: 'Assets' }
   ])
-  t.deepEqual(toQuery('Assets'), [
+  t.deepEqual(toQuery({q: 'Assets'}), [
     { type: 'string', value: 'Assets' }
   ])
-  t.deepEqual(toQuery('acct:"Assets:BDO Savings" Expenses'), [
+  t.deepEqual(toQuery({q: 'acct:"Assets:BDO Savings" Expenses'}), [
     { type: 'key', key: 'acct', value: 'Assets:BDO Savings' },
     { type: 'string', value: 'Expenses' }
   ])
